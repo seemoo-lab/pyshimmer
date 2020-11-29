232cadb050ec490f863d3b9e0ede39b9a6f3f437
Lukas Magel <lmagel@seemoo.tu-darmstadt.de>
2020-11-29 18:19:01 +0100

    Add tests and docstrings for BluetoothSerial

--- pyshimmer/bluetooth/bt_serial.py
+++ pyshimmer/bluetooth/bt_serial.py
@@ -20,16 +20,34 @@ from pyshimmer.serial_base import SerialBase
 
 
 class BluetoothSerial(SerialBase):
+    """Another wrapper layer around the Serial interface
+
+    :arg serial: The :class:`serial.Serial` instance to wrap
+    """
 
     def __init__(self, serial: Serial):
         super().__init__(serial)
 
     def read_varlen(self) -> bytes:
+        """Read the number of bytes specified by the first byte read
+
+        The function reads a single byte that it interprets as a length field with value L and then reads the
+        specified number of L bytes from the stream.
+
+        :return: A variable number of bytes
+        """
         arg_len = self.read_byte()
         arg = self.read(arg_len)
         return arg
 
     def write_varlen(self, arg: bytes) -> None:
+        """Write a variable number of bytes by prepending the data with their length
+
+        The function first writes the length of the data as a single byte, then writes the data itself.
+
+        :param arg: The data to write
+        :raises ValueError: If the number of bytes exceeds the maximum length of 256
+        """
         arg_len = len(arg)
         if arg_len > 255:
             raise ValueError(f'Variable-length argument is too long: {arg_len:d}')
@@ -38,6 +56,13 @@ class BluetoothSerial(SerialBase):
         self.write(arg)
 
     def write_command(self, ccode: int, arg_format: str = None, *args) -> None:
+        """Write a Bluetooth command to the stream
+
+        :param ccode: The code of the command
+        :param arg_format: The argument format, can be a :func:`struct.pack` string or `varlen` for a variable-length
+            argument
+        :param args: The arguments to write along with the command, must meet the requirements of the format string
+        """
         self.write_byte(ccode)
 
         if arg_format is not None:
@@ -47,11 +72,26 @@ class BluetoothSerial(SerialBase):
                 self.write_packed(arg_format, *args)
 
     def read_ack(self) -> None:
+        """Read and assert that the next byte in the stream is an acknowledgment
+
+        :raises ValueError: If the byte is not an acknowledgment
+
+        """
         r = self.read_byte()
         if r != ACK_COMMAND_PROCESSED:
             raise ValueError('Byte received is no acknowledgment')
 
     def read_response(self, rcode: int, arg_format: str = None, instream: bool = False) -> any:
+        """Read a Bluetooth command response from the stream
+
+        :param rcode: The expected response code
+        :param arg_format: The format string to use when decoding the response arguments. Can be None, a
+            :func:`struct.unpack` string or `varlen`. If None, no arguments will be read.
+        :param instream: If the response is an in-stream response. If true, the first byte is expected to be the
+            in-stream flag byte.
+        :raises RuntimeError: If the response code is incorrect
+        :return: The arguments of the response or () if the response has no arguments
+        """
         if instream:
             first_byte = self.read_byte()
             if first_byte != INSTREAM_CMD_RESPONSE:
@@ -66,4 +106,4 @@ class BluetoothSerial(SerialBase):
                 return self.read_varlen()
             else:
                 return self.read_packed(arg_format)
-        return []
+        return ()
